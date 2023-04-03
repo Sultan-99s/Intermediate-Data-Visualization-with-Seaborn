47421fa766be0d1c023dd0a19476edd641044b57
Sultan-99s <93755400+Sultan-99s@users.noreply.github.com>
2023-04-03 23:47:39 +0600

    Create Additional Plot Types.py
    Overview of more complex plot types included in Seaborn.

---
+++ Additional Plot Types.py
@@ -0,0 +1,84 @@
+# Create the stripplot
+sns.stripplot(data=df,
+         x='Award_Amount',
+         y='Model Selected',
+         jitter=True)
+
+plt.show()
+
+
+# Create a boxplot
+sns.boxplot(data=df,
+         x='Award_Amount',
+         y='Model Selected')
+
+plt.show()
+plt.clf()
+
+
+# Show a countplot with the number of models used with each region a different color
+sns.countplot(data=df,
+         y="Model Selected",
+         hue="Region")
+
+plt.show()
+plt.clf()
+
+
+# Display a regression plot for Tuition
+sns.regplot(data=df,
+         y='Tuition',
+         x='SAT_AVG_ALL',
+         marker='^',
+         color='g')
+
+plt.show()
+plt.clf()
+
+
+# Plot a regression plot of Tuition and the Percentage of Pell Grants
+sns.regplot(data=df,
+            y='Tuition',
+            x='PCTPELL')
+
+plt.show()
+plt.clf()
+
+
+# Create a scatter plot by disabling the regression line
+sns.regplot(data=df,
+            y='Tuition',
+            x='UG',
+            fit_reg=False)
+
+plt.show()
+plt.clf()
+
+
+# Create a crosstab table of the data
+pd_crosstab = pd.crosstab(df["Group"], df["YEAR"])
+print(pd_crosstab)
+
+# Plot a heatmap of the table
+sns.heatmap(pd_crosstab)
+
+# Rotate tick marks for visibility
+plt.yticks(rotation=0)
+plt.xticks(rotation=90)
+
+plt.show()
+
+
+# Create the crosstab DataFrame
+pd_crosstab = pd.crosstab(df["Group"], df["YEAR"])
+
+# Plot a heatmap of the table with no color bar and using the BuGn palette
+sns.heatmap(pd_crosstab, cbar=False, cmap="BuGn", linewidths=0.3)
+
+# Rotate tick marks for visibility
+plt.yticks(rotation=0)
+plt.xticks(rotation=90)
+
+#Show the plot
+plt.show()
+plt.clf()
